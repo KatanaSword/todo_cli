141e73e29d3998b334699f8c1d2c1f6711fc464c
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-06-05 18:44:07 +0530

    Initial code structure for todo_cli app

---
+++ todo.py
@@ -0,0 +1,63 @@
+def todos_list():
+    pass
+        
+def complete_todos_list():
+    pass
+
+def incomplete_todos_list():
+    pass
+
+def add_todo(todo, is_todo_complete):
+    pass
+
+def complete_todo(todo_id):
+    pass
+
+def update_todo(todo_id, new_todo):
+    pass
+
+def delete_todo(todo_id):
+    pass
+
+def main():
+    while True:
+        print("\n1. List of all todos.")
+        print("2. List of Complete todos.")
+        print("3. List of Incomplete todos.")
+        print("4. Add todo.")
+        print("5. Mark as complete todo.")
+        print("6. Update todo.")
+        print("7. Delete todo.")
+        print("8. Exit todo app.")
+        choice = input("\nEnter the your choice: ")
+
+        match choice:
+            case "1":
+                todos_list()
+            case "2":
+                complete_todos_list()
+            case "3":
+                incomplete_todos_list()
+            case "4":
+                todo = input("Enter the today todo: ")
+                is_todo_complete = False
+                add_todo(todo, is_todo_complete)
+            case "5":
+                todo_id = input("Enter the todo ID to complete: ")
+                complete_todo(todo_id)
+            case "6":
+                todo_id = input("Enter the todo ID to update: ")
+                new_todo = input("Enter the new today todo: ")
+                update_todo(todo_id, new_todo)
+            case "7":
+                todo_id = input("Enter the todo ID to delete: ")
+                delete_todo(todo_id)
+            case "8":
+                break
+            case _:
+                print("Invalid choice")
+
+
+
+if __name__ == "__main__":
+    main()
